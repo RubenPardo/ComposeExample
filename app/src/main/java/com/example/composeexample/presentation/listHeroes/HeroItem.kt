ce7f153069bd52476cb953f776f1d4c9012aecb1
Ruben Pardo <rubenpardocasanova@gmail.com>
2023-08-03 09:14:57 +0200

    Desarrollo de la feature

--- app/src/main/java/com/example/composeexample/presentation/listHeroes/HeroItem.kt
+++ app/src/main/java/com/example/composeexample/presentation/listHeroes/HeroItem.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
